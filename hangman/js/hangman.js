d9ec78e93d3913077c2f23cf35bee5e07a2b7b89
hkasemir <heidi.kasemir@gmail.com>
2015-09-28 13:38:41 -0700

    style updates

--- hangman/js/hangman.js
+++ hangman/js/hangman.js
@@ -1,127 +1,5 @@
 (function(global){
 
-  var randomWordBaseURL = "http://api.wordnik.com:80/v4/words.json/randomWord?";
-  var randomWordParams = {
-    hasDictionaryDef: "true",
-    excludePartOfSpeech: "proper-noun",
-    minCorpusCount: "20000",
-    minLength: 8,
-    maxLength: 10,
-    api_key: "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"
-  };
-  
-  function makeSVG(tag, attrs) {
-    var el= document.createElementNS('http://www.w3.org/2000/svg', tag);
-    for (var k in attrs)
-      el.setAttribute(k, attrs[k]);
-    return el;
-  };
-
-  
-  var RandomWord = Backbone.Model.extend({
-    defaults: {
-      word: 'example',
-      lettersGuessed: []
-    }
-      
-  });
-  
-  var WordView = Backbone.View.extend({
-    
-    el: 'html',
-        
-    events: {
-      'click div#refresh': 'fetchNewWord',
-      'keydown body': 'checkKey'
-    },
-    
-    initialize: function(){
-      _.bindAll(this, 'render', 'fetchNewWord', 'drawSvg');
-      
-      this.listenTo(this.model, 'change', this.render);
-      this.wordDiv = $('#word');
-      this.guessedDiv = $('#guessed-letters');
-      this.svg = document.getElementById('hangman');
-      this.fetchNewWord();
-    },
-    
-    render: function(){
-      
-      this.word = this.model.get('word');
-      this.guessed = this.model.get('lettersGuessed')
-      var displayText = [];
-      var incorrect = [];
-      var done = true;
-      var lose = false;
-      
-      for (var i=0; i<this.guessed.length; i++){
-        if (this.word.indexOf(this.guessed[i]) < 0){
-          incorrect.push(this.guessed[i]);
-        }
-      };
-      if (incorrect.length > svgParts.length){
-        return;
-      };
-      this.guessedDiv.text(incorrect.join(' '));
-      if (incorrect.length == svgParts.length){
-        this.guessedDiv.append(' - you lose, the word was ' + this.word);
-      };
-      this.drawSvg(incorrect);
-      for (var i=0; i<this.word.length; i++){
-        if (this.guessed.indexOf(this.word[i]) >= 0){
-          displayText.push(this.word[i]);
-        } else {
-          displayText.push('_');
-          done = false;
-        }
-      };
-      this.wordDiv.text(displayText.join(' '));
-      if (done){
-        this.guessedDiv.append(" - WINNER!!");
-        return;
-      }
-    },
-    
-    drawSvg: function(wrongGuesses){
-      while (this.svg.lastChild) {
-        this.svg.removeChild(this.svg.lastChild);
-      };
-      for (var i=0; i < wrongGuesses.length; i++){
-        var newSvg = makeSVG(svgParts[i].type, svgParts[i].attrs);
-        this.svg.appendChild(newSvg);
-      };
-    },
-    
-    fetchNewWord: function(){
-      $.ajax(randomWordBaseURL+$.param(randomWordParams)).done((res) => {
-        if (res.word.indexOf('-') > 0){
-            this.fetchNewWord();
-        } else {
-          this.model.set({ word: res.word.toLowerCase(),
-                           lettersGuessed: [] });
-        };
-      });
-    },
-    
-    checkKey: function(evt){
-
-      var guess = String.fromCharCode(evt.keyCode).toLowerCase();
-      console.log(guess);
-      if (/[a-z]/.test(guess) 
-        && this.guessed.indexOf(guess) < 0
-        && guess.length == 1){
-          this.guessed.push(guess);
-          this.render();
-      }
-    }
-      
-  });
-  
-
-
-  var wordModel = new RandomWord()
-  var wordView = new WordView({model: wordModel});
-  
   var svgParts = [
     {
     type: 'polyline',
@@ -217,6 +95,140 @@
     }
         
   ];
-
   
+  var randomWordBaseURL = "http://api.wordnik.com:80/v4/words.json/randomWord?";
+  var randomWordParams = {
+    hasDictionaryDef: "true",
+    excludePartOfSpeech: "proper-noun",
+    minCorpusCount: "20000",
+    minLength: 8,
+    maxLength: 10,
+    api_key: "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"
+  };
+  
+  function makeSVG(tag, attrs) {
+    var el= document.createElementNS('http://www.w3.org/2000/svg', tag);
+    for (var k in attrs)
+      el.setAttribute(k, attrs[k]);
+    return el;
+  };
+  
+  var RandomWord = Backbone.Model.extend({
+    
+    defaults: {
+      word: 'example',
+      lettersGuessed: []
+    }
+  });
+  
+  var WordView = Backbone.View.extend({
+    
+    el: 'html',
+        
+    events: {
+      'click div#refresh': 'fetchNewWord',
+      'keydown body': 'checkKey'
+    },
+    
+    initialize: function(){
+      _.bindAll(this, 'render', 'fetchNewWord', 'drawSvg');
+      
+      this.listenTo(this.model, 'change', this.render);
+      this.wordDiv = $('#word');
+      this.guessedDiv = $('#guessed-letters');
+      this.svg = document.getElementById('hangman');
+      this.fetchNewWord();
+    },
+    
+    render: function(){
+      
+      this.word = this.model.get('word');
+      this.guessed = this.model.get('lettersGuessed')
+      var displayText = '';
+      var incorrect = [];
+      var done = true;
+      var lose = false;
+      
+      for (var i=0; i<this.guessed.length; i++){
+        if (this.word.indexOf(this.guessed[i]) < 0){
+          incorrect.push(this.guessed[i]);
+        }
+      };
+      
+      if (incorrect.length > svgParts.length){
+        return;
+      };
+      
+      this.guessedDiv.text(incorrect.join(' '));
+      
+      if (incorrect.length == svgParts.length){
+        this.wordDiv.addClass('red');
+        this.guessedDiv.addClass('red');
+        this.guessedDiv.append('<span class="message"> - wrong, the word was "' +
+                               this.word + '"</span>');
+      };
+      
+      this.drawSvg(incorrect);
+      
+      for (var i=0; i<this.word.length; i++){
+        if (this.guessed.indexOf(this.word[i]) >= 0){
+          displayText += this.word[i];
+        } else {
+          displayText += '_';
+          done = false;
+        }
+      };
+      
+      this.wordDiv.text(displayText);
+      
+      if (done){
+        this.wordDiv.addClass("victory");
+        this.guessedDiv.append(" - You win!!");
+        return;
+      }
+    },
+    
+    drawSvg: function(wrongGuesses){
+      
+      while (this.svg.lastChild) {
+        this.svg.removeChild(this.svg.lastChild);
+      };
+      
+      for (var i=0; i < wrongGuesses.length; i++){
+        var newSvg = makeSVG(svgParts[i].type, svgParts[i].attrs);
+        this.svg.appendChild(newSvg);
+      };
+    },
+    
+    fetchNewWord: function(){
+      
+      $.ajax(randomWordBaseURL+$.param(randomWordParams)).done((res) => {
+        if (res.word.indexOf('-') > 0){
+            this.fetchNewWord();
+        } else {
+          this.wordDiv.removeClass("victory");
+          this.guessedDiv.removeClass("red");
+          this.wordDiv.removeClass("red");
+
+          this.model.set({ word: res.word.toLowerCase(),
+                           lettersGuessed: [] });
+        };
+      });
+    },
+    
+    checkKey: function(evt){
+      var guess = String.fromCharCode(evt.keyCode).toLowerCase();
+      
+      if (/[a-z]/.test(guess) 
+        && this.guessed.indexOf(guess) < 0
+        && guess.length == 1){
+          this.guessed.push(guess);
+          this.render();
+      }
+    }
+  });
+  
+  var wordModel = new RandomWord()
+  var wordView = new WordView({model: wordModel});
+
 })(this)
