94ed83fd8ea8174c9607d93318fda5f2b9a847fc
Heidi Kasemir <heidi.kasemir@gmail.com>
2015-10-18 14:11:14 -0700

    refactored to separate views, add fetchword logic to model, and remove button

--- hangman/js/hangman.js
+++ hangman/js/hangman.js
@@ -1,7 +1,204 @@
-(function(global){
+(function (global) {
+  
+  // using Wordink API endpoint with following parameters:
+  
+  var randomWordBaseURL = "http://api.wordnik.com:80/v4/words.json/randomWord?";
+  var randomWordParams = {
+    hasDictionaryDef: "true",
+    excludePartOfSpeech: "proper-noun",
+    minCorpusCount: "20000",
+    minLength: 8,
+    maxLength: 10,
+    api_key: "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"
+  };
+  
+  
+  
+  var RandomWord = Backbone.Model.extend({
+    
+    defaults: {
+      word: 'example',
+      lettersGuessed: [],
+      wrongGuesses: []
+    },
+    
+    initialize: function() {
+      this.fetchNewWord();
+    },
+    
+    fetchNewWord: function(){
+    
+    // uses fat arrow to preserve `this` context!
+      $.ajax(randomWordBaseURL+$.param(randomWordParams)).done((res) => {
+        // remove capitalized words and those with punctuation.
+        if (res.word.search(/[^a-z]/) >= 0){
+            this.fetchNewWord();
+        } else {
 
+          this.set({ 
+            word: res.word,
+            lettersGuessed: [],
+            wrongGuesses: []
+          });
+        };
+      });
+    }
+  });
+  
+  
+  // MainView will handle the keypresses and most of the game logic.
+//  It will also render a loss or win.
+//  It has 2 child views, one that renders the SVG for the hangman,
+//  the other renders the word and letters guessed.
+  
+  var MainView = Backbone.View.extend({
+    
+    el: 'html',
+            
+    events: {
+      'keydown body': 'checkKey'
+    },
+    
+    initialize: function() {
+      this.listenTo(this.model, 'change', this.resetWord);
+      this.resultsDiv = $('#results');
+      this.wordDiv = $('#word');
+    },
+    
+    resetWord: function(){
+      this.word = this.model.get('word');
+      this.guessed = this.model.get('lettersGuessed');
+      this.wrong = this.model.get('wrongGuesses');
+      this.wordDiv.removeClass('red victory');
+      this.resultsDiv.text('');
+    },
+    
+    checkKey: function(evt){
+      // fetch a new word by pressing 'enter'
+      if (evt.keyCode == 13){
+          this.model.fetchNewWord();
+      }
+      // Check if round is won or lost. If so, return without doing anything.
+      if (this.wrong.length == svgParts.length){
+        return
+      }
+      
+      if (this.wordDiv.text().search('_') < 0){
+        return
+      }
+      
+      // check if keypress is a letter:
+      var guess = String.fromCharCode(evt.keyCode).toLowerCase();
+      
+      if (/[a-z]/.test(guess) 
+        && this.guessed.indexOf(guess) < 0
+        && guess.length == 1){
+          this.guessed.push(guess);
+        
+        // if the letter is not in the word,
+        // push it to the wrong guesses array:
+        if(this.word.search(guess) < 0 ) {
+          this.wrong.push(guess);
+        };
+        // set variables in the model
+        this.model.set({
+          lettersGuessed: this.guessed,
+          wrongGuesses: this.wrong
+        });
+        // for some reason this is not triggering a change event on the model, so manually trigger:
+        this.model.trigger('change:lettersGuessed');
+
+      }
+      
+//      render win or lose
+      if (this.wrong.length == svgParts.length) {
+        this.renderLoss();
+        return
+      }
+
+      if (this.wordDiv.text().search('_') < 0) {
+        this.renderWin();
+        return
+      }
+    },
+    
+    renderWin: function(){
+      this.wordDiv.addClass('victory')
+      this.resultsDiv.text('You win!!');
+    },
+    
+    renderLoss: function(){
+      this.wordDiv.addClass('red');
+      this.resultsDiv.text('Wrong, the word was "' +
+                               this.word + '"');
+    }
+    
+  })
+  
+  var SVGView = Backbone.View.extend({
+    
+    initialize: function() {
+      _.bindAll(this, 'render');
+
+//      When a guess is made, re-render the SVG per the wrong guesses array.
+      this.listenTo(this.model, 'change:lettersGuessed', this.render);
+      this.svg = document.getElementById('hangman');
+    },
+    
+    render: function(){
+      while (this.svg.lastChild) {
+        this.svg.removeChild(this.svg.lastChild);
+      };
+      
+      for (var i=0; i < this.model.get('wrongGuesses').length; i++){
+        var newSvg = makeSVG(svgParts[i].type, svgParts[i].attrs);
+        this.svg.appendChild(newSvg);
+      };
+    },
+    
+  })
+  
+  
+  var WordView = Backbone.View.extend({
+    
+    
+    initialize: function() {
+      _.bindAll(this, 'render');
+      
+      this.listenTo(this.model, 'change', this.render);
+      this.listenTo(this.model, 'change:lettersGuessed', this.render);
+      this.wordDiv = $('#word');
+      this.guessedDiv = $('#guessed-letters');
+      this.render();
+    },
+    
+    render: function(){
+      
+      this.word = this.model.get('word');
+      this.guessed = this.model.get('lettersGuessed')
+      this.wrong = this.model.get('wrongGuesses')
+      var displayText = '';
+      
+      this.guessedDiv.text(this.wrong.join(' '));
+      
+      for (var i=0; i<this.word.length; i++){
+        if (this.guessed.indexOf(this.word[i]) >= 0){
+          displayText += this.word[i];
+        } else {
+          displayText += '_';
+        }
+      };
+      
+      this.wordDiv.text(displayText);
+    },
+    
+
+  });
+  
   var svgParts = [
+
     {
+
     type: 'polyline',
       attrs: {
         points: '190,180 10,180 40,180 40,10 40,30 60,10 37.5,10 142.5,10',
@@ -30,7 +227,7 @@
         'stroke-width': '5',
         cx: 140,
         cy: 50,
-        r: 10,
+        r: 10
       }
     },
     {
@@ -93,146 +290,19 @@
         y2: 95
       }
     }
-        
+
   ];
-  
-  var randomWordBaseURL = "http://api.wordnik.com:80/v4/words.json/randomWord?";
-  var randomWordParams = {
-    hasDictionaryDef: "true",
-    excludePartOfSpeech: "proper-noun",
-    minCorpusCount: "20000",
-    minLength: 8,
-    maxLength: 10,
-    api_key: "a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5"
-  };
-  
+
   function makeSVG(tag, attrs) {
-    var el= document.createElementNS('http://www.w3.org/2000/svg', tag);
+    var el = document.createElementNS('http://www.w3.org/2000/svg', tag);
     for (var k in attrs)
       el.setAttribute(k, attrs[k]);
     return el;
   };
   
-  var RandomWord = Backbone.Model.extend({
-    
-    defaults: {
-      word: 'example',
-      lettersGuessed: []
-    }
-  });
-  
-  var WordView = Backbone.View.extend({
-    
-    el: 'html',
-        
-    events: {
-      'click div#refresh': 'fetchNewWord',
-      'keydown body': 'checkKey'
-    },
-    
-    initialize: function(){
-      _.bindAll(this, 'render', 'fetchNewWord', 'drawSvg');
-      
-      this.listenTo(this.model, 'change', this.render);
-      this.wordDiv = $('#word');
-      this.guessedDiv = $('#guessed-letters');
-      this.svg = document.getElementById('hangman');
-      this.fetchNewWord();
-    },
-    
-    render: function(){
-      
-      if (this.wordDiv.hasClass('red') ||
-          this.wordDiv.hasClass('victory')){
-        return;
-      };
-      
-      this.word = this.model.get('word');
-      this.guessed = this.model.get('lettersGuessed')
-      var displayText = '';
-      var incorrect = [];
-      var done = true;
-      
-      for (var i=0; i<this.guessed.length; i++){
-        if (this.word.indexOf(this.guessed[i]) < 0){
-          incorrect.push(this.guessed[i]);
-        }
-      };
-      
-      this.guessedDiv.text(incorrect.join(' '));
-      
-      if (incorrect.length == svgParts.length){
-        this.wordDiv.addClass('red');
-        this.guessedDiv.addClass('red');
-        this.guessedDiv.append('<p class="message">Wrong, the word was "' +
-                               this.word + '"</p>');
-      };
-      
-      this.drawSvg(incorrect);
-      
-      for (var i=0; i<this.word.length; i++){
-        if (this.guessed.indexOf(this.word[i]) >= 0){
-          displayText += this.word[i];
-        } else {
-          displayText += '_';
-          done = false;
-        }
-      };
-      
-      this.wordDiv.html('<p>' + displayText + '<p>');
-      
-      if (done){
-        this.wordDiv.addClass("victory");
-
-        this.guessedDiv.append('<p class="message">You win!!</p>');
-        return;
-      }
-    },
-    
-    drawSvg: function(wrongGuesses){
-      
-      while (this.svg.lastChild) {
-        this.svg.removeChild(this.svg.lastChild);
-      };
-      
-      for (var i=0; i < wrongGuesses.length; i++){
-        var newSvg = makeSVG(svgParts[i].type, svgParts[i].attrs);
-        this.svg.appendChild(newSvg);
-      };
-    },
-    
-    fetchNewWord: function(){
-      
-      $.ajax(randomWordBaseURL+$.param(randomWordParams)).done((res) => {
-        if (res.word.search(/[^a-z]/) >= 0){
-          // effectively removes all capitalized words and those with punctuation.
-            this.fetchNewWord();
-        } else {
-          this.wordDiv.removeClass("victory");
-          this.wordDiv.removeClass("red");
-//          console.log(res.word);
-
-          this.model.set({ word: res.word.toLowerCase(),
-                           lettersGuessed: [] });
-        };
-      });
-    },
-    
-    checkKey: function(evt){
-      var guess = String.fromCharCode(evt.keyCode).toLowerCase();
-      if (/[a-z]/.test(guess) 
-        && this.guessed.indexOf(guess) < 0
-        && guess.length == 1){
-          this.guessed.push(guess);
-          this.render();
-      }
-      if (evt.keyCode == 13){
-          this.fetchNewWord();
-      }
-    }
-  });
-  
   var wordModel = new RandomWord()
+  var mainView = new MainView({model: wordModel});
+  var svgView = new SVGView({model: wordModel});
   var wordView = new WordView({model: wordModel});
 
 })(this)
